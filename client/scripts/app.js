9069383638a74d713fc955eda2f70883688e2fa2
philipjrosen <philipjrosen@gmail.com>
2014-12-29 14:41:11 -0800

    added ajax skeleton

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,16 @@
 // YOUR CODE HERE:
 
+$.ajax({
+  // always use this url
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+});
