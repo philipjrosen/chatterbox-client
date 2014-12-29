f7e9be6d02c7bdeb279e6ce45ee66c3b10d53981
philipjrosen <philipjrosen@gmail.com>
2014-12-29 15:54:26 -0800

    add button to get new chats

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,20 @@
-$.ajax({
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'GET',
-  success: function (data) {
-    displayMessages(data);
-  },
-  error: function (data) {
-    console.error('chatterbox: Failed to send message');
-  }
-});
 
-var displayMessages = function(data) {
+var getChats = function(successCallback) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    success: function (data) {
+      successCallback(data.results);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+}
+
+var displayMessages = function(messages) {
   var $ul = $('.chatList');
-  _.each(data.results, function(message){
+  _.each(messages, function(message){
     var $li = $('<li class="post"></li>');
     var $user = $('<p class=user></p>');
     var $message = $('<p class="message"></p>');
@@ -21,3 +24,22 @@ var displayMessages = function(data) {
     $ul.append($li);
   });
 }
+
+var displayNewMessages = function(messages) {
+  var $length = $('li').length;
+  var results = messages.slice($length);
+  if ( !results.length ) {
+    console.log('displayNewMessages')
+  }
+  displayMessages(results);
+};
+
+$(document).ready(function(){
+  getChats(displayMessages);
+
+  $('button').on('click', function(){
+    getChats(displayNewMessages);
+  });
+
+});
+
