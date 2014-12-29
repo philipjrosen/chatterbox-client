90442c67e602a9bbec2b20c6d01c75b0be99d6e9
philipjrosen <philipjrosen@gmail.com>
2014-12-29 14:56:09 -0800

    get data from server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,10 +4,8 @@ $.ajax({
   // always use this url
   url: 'https://api.parse.com/1/classes/chatterbox',
   type: 'GET',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
   success: function (data) {
-    console.log('chatterbox: Message sent');
+    console.log(data);
   },
   error: function (data) {
     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
