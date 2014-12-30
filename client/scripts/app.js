f8b187b9850c8076a91ea8bd28f10147c849d2c3
philipjrosen <philipjrosen@gmail.com>
2014-12-30 11:04:21 -0800

    deletes commented code, refactors use of app.server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,7 +7,7 @@ window.app = {
 
   send: function(message) {
     $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
+      url: app.server,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
@@ -22,10 +22,9 @@ window.app = {
 
   fetch: function() {
     var lastPostTime = $('li span').first().text() || '2014-12-01T00:00:00';
-    var url = 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&where={"createdAt":{"$gt":"'+lastPostTime+'"}}';
-    // note: need to account for query options in ajax call
     $.ajax({
       url: app.server,
+      data: 'order=-createdAt&where={"createdAt":{"$gt":"'+lastPostTime+'"}}',
       type: 'GET',
       success: function (data) {
         app.displayMessages(data.results);
@@ -52,55 +51,6 @@ window.app = {
     });
   }
 };
-
-
-// var getChats = function(successCallback) {
-//   var lastPostTime = $('li span').first().text() || '2014-12-01T00:00:00';
-//   var url = 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&where={"createdAt":{"$gt":"'+lastPostTime+'"}}';
-
-//   $.ajax({
-//     url: url,
-//     type: 'GET',
-//     success: function (data) {
-//       successCallback(data.results);
-//     },
-//     error: function (data) {
-//       console.error('chatterbox: Failed to send message');
-//     }
-//   });
-// }
-
-// var postChat = function(message) {
-//   $.ajax({
-//     url: 'https://api.parse.com/1/classes/chatterbox',
-//     type: 'POST',
-//     data: JSON.stringify(message),
-//     contentType: 'application/json',
-//     success: function (data) {
-//       console.log("post:", data);
-//     },
-//     error: function (data) {
-//       console.error('chatterbox: Failed to send message');
-//     }
-//   });
-// };
-
-// var displayMessages = function(messages) {
-//   var arePosts = !!$('.post').length;
-//   console.log(arePosts);
-//   var $ul = $('.chatList');
-//   _.each(messages, function(message){
-//     var $li = $('<li class="post"></li>');
-//     var $user = $('<p class=user></p>');
-//     var $message = $('<p class="message"></p>');
-//     var $createdAt = $('<span class="createdAt"></span>');
-//     $user.text(message.username);
-//     $message.text(message.text);
-//     $createdAt.text(message.createdAt);
-//     $li.append($user, $message, $createdAt);
-//     arePosts ? $ul.prepend($li) : $ul.append($li);
-//   });
-// };
 
 $(document).ready(function(){
   app.init();
