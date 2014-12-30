c9625c97af9ce147eecf0391b2c961550f43c5a0
philipjrosen <philipjrosen@gmail.com>
2014-12-30 10:38:54 -0800

    send correctly does post request

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,7 +2,7 @@ window.app = {
   server: 'https://api.parse.com/1/classes/chatterbox',
 
   init: function(){
-
+    app.fetch();
   },
 
   send: function(message) {
@@ -54,59 +54,59 @@ window.app = {
 };
 
 
-var getChats = function(successCallback) {
-  var lastPostTime = $('li span').first().text() || '2014-12-01T00:00:00';
-  var url = 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&where={"createdAt":{"$gt":"'+lastPostTime+'"}}';
+// var getChats = function(successCallback) {
+//   var lastPostTime = $('li span').first().text() || '2014-12-01T00:00:00';
+//   var url = 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&where={"createdAt":{"$gt":"'+lastPostTime+'"}}';
 
-  $.ajax({
-    url: url,
-    type: 'GET',
-    success: function (data) {
-      successCallback(data.results);
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-}
+//   $.ajax({
+//     url: url,
+//     type: 'GET',
+//     success: function (data) {
+//       successCallback(data.results);
+//     },
+//     error: function (data) {
+//       console.error('chatterbox: Failed to send message');
+//     }
+//   });
+// }
 
-var postChat = function(message) {
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log("post:", data);
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-};
+// var postChat = function(message) {
+//   $.ajax({
+//     url: 'https://api.parse.com/1/classes/chatterbox',
+//     type: 'POST',
+//     data: JSON.stringify(message),
+//     contentType: 'application/json',
+//     success: function (data) {
+//       console.log("post:", data);
+//     },
+//     error: function (data) {
+//       console.error('chatterbox: Failed to send message');
+//     }
+//   });
+// };
 
-var displayMessages = function(messages) {
-  var arePosts = !!$('.post').length;
-  console.log(arePosts);
-  var $ul = $('.chatList');
-  _.each(messages, function(message){
-    var $li = $('<li class="post"></li>');
-    var $user = $('<p class=user></p>');
-    var $message = $('<p class="message"></p>');
-    var $createdAt = $('<span class="createdAt"></span>');
-    $user.text(message.username);
-    $message.text(message.text);
-    $createdAt.text(message.createdAt);
-    $li.append($user, $message, $createdAt);
-    arePosts ? $ul.prepend($li) : $ul.append($li);
-  });
-};
+// var displayMessages = function(messages) {
+//   var arePosts = !!$('.post').length;
+//   console.log(arePosts);
+//   var $ul = $('.chatList');
+//   _.each(messages, function(message){
+//     var $li = $('<li class="post"></li>');
+//     var $user = $('<p class=user></p>');
+//     var $message = $('<p class="message"></p>');
+//     var $createdAt = $('<span class="createdAt"></span>');
+//     $user.text(message.username);
+//     $message.text(message.text);
+//     $createdAt.text(message.createdAt);
+//     $li.append($user, $message, $createdAt);
+//     arePosts ? $ul.prepend($li) : $ul.append($li);
+//   });
+// };
 
 $(document).ready(function(){
-  getChats(displayMessages);
+  app.init();
 
   $('button').on('click', function(){
-    getChats(displayMessages);
+    app.fetch();
   });
 
   $('.postMessage').on('submit', function(e) {
@@ -120,7 +120,7 @@ $(document).ready(function(){
     message['roomname'] = 'defaultRoomName';
     $text.val('');
     $username.val('');
-    postChat(message);
+    app.send(message);
   });
 
 });
